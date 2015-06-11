4bd6aeb2e5ada867a92d2ab84b193925e8770d87
JonathanScrivanich <Jonathan.Scrivanich@gmail.com>
2015-06-11 19:16:32 -0400

    Fixed name
    ...

--- waterjugpuzzle.cpp
+++ waterjugpuzzle.cpp
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Name        : waterjugpuzzle.cpp
- * Author      : Kevin Furlong, Jonathan Scrivan, and Henry Thomas
+ * Author      : Kevin Furlong, Jonathan Scrivanich, and Henry Thomas
  * Date        : June 12th 2015
  * Description : Determining the most efficient way to solve the classic water
  * 				 jug problem. Given a goal and jug capacities.
  * Pledge      : We pledge our honor that we have abided by the Stevens Honor System
- * 				 Kevin Furlong, Henry Thomas, Jonathan Scrivan
+ * 				 Kevin Furlong, Henry Thomas, Jonathan Scrivanich
  ******************************************************************************/
 #include <iostream>
 #include <vector>
