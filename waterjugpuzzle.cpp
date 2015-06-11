f667d33eaeaa5d2b7613dcb574ee25bc0a710081
Hanky <henryhunter23@gmail.com>
2015-06-11 19:28:45 -0400

    Added TODO's for some stuff

--- waterjugpuzzle.cpp
+++ waterjugpuzzle.cpp
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Name        : waterjugpuzzle.cpp
- * Author      : Kevin Furlong, Jonathan Scrivanich, and Henry Thomas
+ * Author      : Kevin Furlong, Jonathan Scrivan, and Henry Thomas
  * Date        : June 12th 2015
  * Description : Determining the most efficient way to solve the classic water
  * 				 jug problem. Given a goal and jug capacities.
  * Pledge      : We pledge our honor that we have abided by the Stevens Honor System
- * 				 Kevin Furlong, Henry Thomas, Jonathan Scrivanich
+ * 				 Kevin Furlong, Henry Thomas, Jonathan Scrivan
  ******************************************************************************/
 #include <iostream>
 #include <vector>
@@ -49,12 +49,14 @@ vector<State> findPath(State &child) {
 }
 
 void createAdjacencies(State &curr) {
+	//TODO: Possibly set capacity for visited && fill visited with false
 	int a = curr.a(), b = curr.b(), c = curr.c();
 	//C->A
 	if (c != 0 && a != cap.a()) {
 		int amtMoved = min(c, (cap.a() - a));
 		if (!visited[a + amtMoved][b])
 			q.push(new State(a + amtMoved, b, c - amtMoved, curr));
+			//TODO: Update visited to true
 	}
 }
 
