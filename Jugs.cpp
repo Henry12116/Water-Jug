27cd0e8db92d995c50eef2d636e2f63b58457f05
Hanky <henryhunter23@gmail.com>
2015-06-11 16:48:27 -0400

    Just getting started

---
+++ Jugs.cpp
@@ -0,0 +1,21 @@
+/*
+ * Jugs.cpp
+ *
+ *  Created on: Jun 8, 2015
+ *      Author: Henry Thomas
+ *      Pledge: I pledge my honor that I have abided by the Stevens Honor System.
+ */
+#include <iostream>
+#include <>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	//some error checking
+	if (argc != 6) {
+		cout << "Usage: ./waterjugpuzzle <cap A> <cap B> <cap C> <goal A> <goal B> <goal C>" << endl;
+		return 0;
+	}
+}
+
+
